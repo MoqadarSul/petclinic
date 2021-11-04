13c8ca89d33f7b94cc03b82a5688dcb44fbcac91
MoqadarSul <45808105+MoqadarSul@users.noreply.github.com>
2021-11-04 02:53:47 -0400

    added controller and thymleaf data.

--- pet-clinic-web/src/main/java/ca/gbc/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ca/gbc/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package ca.gbc.petclinic.controllers;
 
+import ca.gbc.petclinic.Services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-        @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-        public String listVets(){
+        private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+        public String listVets(Model model){
+            model.addAttribute("vets", vetService.findAll());
             return "vet/index";
         }
     }
